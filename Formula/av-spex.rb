f73c5faa5a3b229fdff3be1d5249a787fa938781
eddycollotonn <eddy.colloton@gmail.com>
2025-02-12 12:29:59 -0700

    adding ninja to dependencies

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -11,6 +11,8 @@ class AvSpex < Formula
   depends_on "qt@6"
   depends_on "numpy"
   depends_on "pkg-config"
+  depends_on "ninja" => :build
+
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/92/ec/089608b791d210aec4e7f97488e67ab0d33add3efccb83a056cbafe3a2a6/setuptools-75.8.0.tar.gz"
