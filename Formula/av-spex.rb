6c0eb6bb7d8131b8bcf28a37f97382d72797f3e9
eddycollotonn <eddy.colloton@gmail.com>
2025-02-13 09:17:05 -0700

    troubleshooting pyqt6, adding depends_on qt@6

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -8,6 +8,7 @@ class AvSpex < Formula
   license "GPL-3.0-only"
 
   depends_on "python@3.10"
+  epends_on "qt@6"
 
   resource "toml" do
     url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
