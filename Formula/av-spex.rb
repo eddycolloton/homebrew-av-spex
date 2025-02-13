44ab4a202d378cdfead8ea86d4ea931153343810
eddycollotonn <eddy.colloton@gmail.com>
2025-02-13 09:03:03 -0700

    troubleshooting pyqt6

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -8,55 +8,6 @@ class AvSpex < Formula
   license "GPL-3.0-only"
 
   depends_on "python@3.10"
-  depends_on "pyqt@6"
-  depends_on "numpy"
-  depends_on "cython"
-  depends_on "meson"
-
-  resource "meson-python" do
-    url "https://files.pythonhosted.org/packages/67/66/91d242ea8dd1729addd36069318ba2cd03874872764f316c3bb51b633ed2/meson_python-0.17.1.tar.gz"
-    sha256 "efb91f69f2e19eef7bc9a471ed2a4e730088cc6b39eacaf3e49fc4f930eb5f83"
-  end
-
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/92/ec/089608b791d210aec4e7f97488e67ab0d33add3efccb83a056cbafe3a2a6/setuptools-75.8.0.tar.gz"
-    sha256 "c5afc8f407c626b8313a86e10311dd3f661c6cd9c09d4bf8c15c0e11f9f2b0e6"
-  end
-
-  resource "wheel" do
-    url "https://files.pythonhosted.org/packages/8a/98/2d9906746cdc6a6ef809ae6338005b3f21bb568bea3165cfc6a243fdc25c/wheel-0.45.1.tar.gz"
-    sha256 "661e1abd9198507b1409a20c02106d9670b2576e916d58f520316666abca6729"
-  end
-
-  resource "appdirs" do
-    url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
-    sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
-  end
-
-  resource "colorlog" do
-    url "https://files.pythonhosted.org/packages/78/6b/4e5481ddcdb9c255b2715f54c863629f1543e97bc8c309d1c5c131ad14f2/colorlog-6.7.0.tar.gz"
-    sha256 "bd94bd21c1e13fac7bd3153f4bc3a7dc0eb0974b8bc2fdf1a989e474f6e582e5"
-  end
-
-  resource "art" do
-    url "https://files.pythonhosted.org/packages/b6/15/6c4ac6bf544a01230bad5b45ce4f624051b9dc9567875da05cfdbfc2cafa/art-6.1.tar.gz"
-    sha256 "6ab3031e3b7710039e73497b0e750cadfe04d4c1279ce3a123500dbafb9e1b64"
-  end
-
-  resource "lxml" do
-    url "https://files.pythonhosted.org/packages/ef/f6/c15ca8e5646e937c148e147244817672cf920b56ac0bf2cc1512ae674be8/lxml-5.3.1.tar.gz"
-    sha256 "106b7b5d2977b339f1e97efe2778e2ab20e99994cbb0ec5e55771ed0795920c8"
-  end
-
-  resource "pandas" do
-    url "https://files.pythonhosted.org/packages/88/d9/ecf715f34c73ccb1d8ceb82fc01cd1028a65a5f6dbc57bfa6ea155119058/pandas-2.2.2.tar.gz"
-    sha256 "9e79019aba43cb4fda9e4d983f8e88ca0373adbb697ae9c6c43093218de28b54"
-  end
-
-  resource "toml" do
-    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
-    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
-  end
 
   resource "PyQt6" do
     url "https://files.pythonhosted.org/packages/d1/f9/b0c2ba758b14a7219e076138ea1e738c068bf388e64eee68f3df4fc96f5a/PyQt6-6.7.1.tar.gz"
@@ -64,56 +15,7 @@ class AvSpex < Formula
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3.10", system_site_packages: true)
-    
-    # Upgrade pip first
-    venv.pip_install "pip"
-    
-    # Install build dependencies
-    venv.pip_install "wheel"
-    venv.pip_install "setuptools"
-    venv.pip_install "versioneer[toml]"
-    venv.pip_install resource("meson-python")
-    
-    # Install all resources except PyQt6 first
-    resources.reject { |r| r.name == "PyQt6" }.each do |r|
-      r.stage do
-        venv.pip_install Pathname.pwd
-      end
-    end
-    
-    # Install PyQt6 with license acceptance
-    resource("PyQt6").stage do
-      system "#{libexec}/bin/python", "-m", "pip", "install", ".", 
-             "--config-settings", "license_accepted=yes",
-             "--no-deps",
-             "--ignore-installed",
-             "--prefix=#{libexec}"
-    end
-    
-    # Install the main package
-    cd buildpath do
-      system "#{libexec}/bin/python", "-m", "pip", "install", ".", 
-             "--no-deps",
-             "--ignore-installed",
-             "--prefix=#{libexec}",
-             "--no-binary", ":all:",
-             "--verbose"
-    end
-  
-    # Rest of the installation remains the same...
-    pyqt = Formula["pyqt@6"].opt_prefix
-    site_packages = "#{HOMEBREW_PREFIX}/lib/python3.10/site-packages"
-    
-    env = {
-      "PATH" => "#{libexec}/bin:$PATH",
-      "PYTHONPATH" => "#{libexec}/lib/python3.10/site-packages:#{site_packages}:#{pyqt}/lib/python3.10/site-packages",
-      "QT_PLUGIN_PATH" => "#{pyqt}/share/qt/plugins"
-    }
-  
-    %w[av-spex av-spex-gui].each do |f|
-      (bin/f).write_env_script(libexec/"bin"/f, env)
-    end
+    virtualenv_install_with_resources
   end
 
   test do
