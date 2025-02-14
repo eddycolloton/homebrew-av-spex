e625ce4fe3525a1029020f3d283309a7a329577a
eddycollotonn <eddy.colloton@gmail.com>
2025-02-14 16:36:09 -0700

    last commit completes successfully! adding lxml back to resources

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -34,6 +34,11 @@ class AvSpex < Formula
     sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
   end
 
+  resource "lxml" do
+    url "https://files.pythonhosted.org/packages/ef/f6/c15ca8e5646e937c148e147244817672cf920b56ac0bf2cc1512ae674be8/lxml-5.3.1.tar.gz"
+    sha256 "106b7b5d2977b339f1e97efe2778e2ab20e99994cbb0ec5e55771ed0795920c8"
+  end
+
   resource "PyQt6" do
     url "https://files.pythonhosted.org/packages/d1/f9/b0c2ba758b14a7219e076138ea1e738c068bf388e64eee68f3df4fc96f5a/PyQt6-6.7.1.tar.gz"
     sha256 "3672a82ccd3a62e99ab200a13903421e2928e399fda25ced98d140313ad59cb9"
