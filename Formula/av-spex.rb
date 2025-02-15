d72f5a600dde791b373258947b14f45ed8d78bb9
eddycollotonn <eddy.colloton@gmail.com>
2025-02-14 17:41:12 -0700

    adding plotly back in

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -47,6 +47,11 @@ class AvSpex < Formula
     sha256 "9e79019aba43cb4fda9e4d983f8e88ca0373adbb697ae9c6c43093218de28b54"
   end
 
+  resource "plotly" do
+    url "https://files.pythonhosted.org/packages/db/9e/31b2f0b8f2357cd5f3e992c76c3e4e85a5cbbad8b8c5f23d0684e3f4c608/plotly-5.23.0.tar.gz"
+    sha256 "89e57d003a116303a34de6700862391367dd564222ab71f8531df70279fc0193"
+  end
+
   resource "PyQt6" do
     url "https://files.pythonhosted.org/packages/d1/f9/b0c2ba758b14a7219e076138ea1e738c068bf388e64eee68f3df4fc96f5a/PyQt6-6.7.1.tar.gz"
     sha256 "3672a82ccd3a62e99ab200a13903421e2928e399fda25ced98d140313ad59cb9"
