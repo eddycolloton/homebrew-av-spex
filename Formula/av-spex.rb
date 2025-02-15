2a2b9af622fea9ec93bdff7b93f70132032fccc3
eddycollotonn <eddy.colloton@gmail.com>
2025-02-14 17:53:11 -0700

    forgot to add dpends_on statement

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -11,6 +11,7 @@ class AvSpex < Formula
   depends_on "numpy" => :build # needed for lxml
   depends_on "cmake" => :build # needed for pandas
   depends_on "ninja" => :build # needed for pandas
+  depends_on "jupyterlab" => :build
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/92/ec/089608b791d210aec4e7f97488e67ab0d33add3efccb83a056cbafe3a2a6/setuptools-75.8.0.tar.gz"
