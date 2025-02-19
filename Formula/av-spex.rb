1f2ec0312cb9e744f112f9db06ba7c2b1ad92c49
eddycollotonn <eddy.colloton@gmail.com>
2025-02-19 09:55:11 -0700

    bottle code causing issues

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -8,17 +8,6 @@ class AvSpex < Formula
   sha256 "5436a72a982fc1a4d90c0b7ddda7adf51438eed1f3817a00ebdb26e937d8e69f"
   license "GPL-3.0-only"
 
-  bottle do
-    rebuild 0  
-    sha256 cellar: :any,  
-      arm64_sonoma: "TO_BE_FILLED",
-      arm64_ventura: "TO_BE_FILLED",
-      arm64_monterey: "TO_BE_FILLED",
-      sonoma: "TO_BE_FILLED",
-      ventura: "TO_BE_FILLED",
-      monterey: "TO_BE_FILLED"
-  end
-
   depends_on "python@3.10" => :build
   depends_on "numpy" => :build # needed for lxml
   
