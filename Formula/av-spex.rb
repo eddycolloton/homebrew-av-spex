6908a8af733ac79f38f1d67d7982641d208a8bfe
eddycollotonn <eddy.colloton@gmail.com>
2025-02-20 10:49:51 -0700

    confirming removing pyqt6 fixes broken links when pyhton3 is a runtime deps

--- Formula/av-spex.rb
+++ Formula/av-spex.rb
@@ -66,12 +66,6 @@ class AvSpex < Formula
     # Install all Python dependencies including PyQt6-sip but excluding PyQt6
     venv.pip_install resources.reject { |r| r.name == "PyQt6" || r.name == "plotly" }
     
-    # Install PyQt6 with necessary dependencies
-    system libexec/"bin/python", "-m", "pip", "install", "--only-binary", ":all:",
-           "--config-settings", "--confirm-license=", "--verbose", "PyQt6==6.7.1"
-
-    # Remove the problematic SQL driver plugin
-    rm_f libexec/"lib/python3.10/site-packages/PyQt6/Qt6/plugins/sqldrivers/libqsqlmimer.dylib"
 
     # Install the package itself
     venv.pip_install_and_link buildpath
